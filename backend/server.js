ad6638ae29aa489cf5e413f8090ec4c4535b2561
Roha-n6002 <talasanirohan006@gmail.com>
2025-04-24 16:07:59 +0530

    Trial change

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.use('/',authRouter);
 const port = process.env.PORT || 5000;
 
 app.listen(port,() => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server has started on port ${port}`);
 });
